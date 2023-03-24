a7f5be71c3c8858555e99038518252a3fcdddcfb
Rhapsody0128 <rhapsody0128@gmail.com>
2023-03-25 03:35:24 +0800

    建立角色按鈕互動FIN

--- Lineage/Assets/Scripts/CreateRoleBtn.cs
+++ Lineage/Assets/Scripts/CreateRoleBtn.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using RoleSystem;
+using TMPro;
 
 public class CreateRoleBtn : MonoBehaviour
 {
-
-    public void CreateRole(){
+    public TMP_Text  textField ; 
+    public void CreateRole() {
         var role = RoleController.getRandomRole() ;
-        Debug.Log(role.name) ;
+        // GameObject obj = GameObject.Find("RoleInfo") ;
+        // TMP_Text  text = obj.GetComponent<TMP_Text >();
+        string str = @$"姓名 {role.name}·{role.lastName}
+力量 {role.strength}
+敏捷 {role.agility}
+體質 {role.vitality}
+靈巧 {role.dexterity}
+智慧 {role.intelligence}
+信仰 {role.mentality}";
+        textField.text = str ; 
     }
 }

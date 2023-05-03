f07b66ff1a05a417609ec9286e57189584a8ea86
gary_lai <gary_lai@auer.com.tw>
2023-05-03 15:52:10 +0800

    測試run,debug

--- Lineage/Assets/Scripts/CreateRoleBtn.cs
+++ Lineage/Assets/Scripts/CreateRoleBtn.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using RoleSystem;
+using HeroSystem;
 using TMPro;
 using UtilSystem;
 
-public class CreateRoleBtn : MonoBehaviour
+public class CreateHeroBtn : MonoBehaviour
 {
     public TMP_Text  content; 
     public TMP_Text  title ; 
-    public void CreateRole() {
-        var role = RoleController.getRandomRole() ;
-        // GameObject obj = GameObject.Find("RoleInfo") ;
+    public void CreateHero() {
+        var hero = HeroController.getRandomHero() ;
+        // GameObject obj = GameObject.Find("HeroInfo") ;
         // TMP_Text  text = obj.GetComponent<TMP_Text >();
-        title.text = @$"{role.name}·{role.lastName}" ;
-        content.text = $"力量 {role.strength}  敏捷 {role.agility}\n體質 {role.vitality}  靈巧 {role.dexterity}\n智慧 {role.intelligence}  信仰 {role.mentality}";
+        title.text = @$"{hero.name}·{hero.lastName}" ;
+        content.text = $"力量 {hero.strength}  敏捷 {hero.agility}\n體質 {hero.vitality}  靈巧 {hero.dexterity}\n智慧 {hero.intelligence}  信仰 {hero.mentality}";
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         sphere.transform.position = new Vector3(Util.getRandom(0,400),Util.getRandom(0,200),0);
         sphere.transform.localScale  = new Vector3(40, 40, 40);
